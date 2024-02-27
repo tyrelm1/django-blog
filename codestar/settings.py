e2d1c469265a3ff08d2f3f9963ff7a86da94e5d4
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-02-27 20:30:29 +0000

    commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-rm&l_@2q(s9dx968+rcowz-hodd%)y_y#xx5+#n_^j1x%=4y8f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = ['8000-tyrelm1-djangoblog-stbbj06oirw.ws-eu108.gitpod.io',
-                 '.herokuapp.com'
+                 '.herokuapp.com',
                 ]
 
 
