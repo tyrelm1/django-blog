b76eb1c6c0ba9f74d61a50d8ef5e1b3a5b81c164
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-02-27 22:55:28 +0000

    created comments model

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                            related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
