a90b385d3a4dc2e86020f94ad9ada72f02fc6204
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-02-29 15:12:55 +0000

    added_post detail view

--- blog/views.py
+++ blog/views.py
@@ -23,12 +23,7 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
 
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
-    )
+    return render(request,"blog/post_detail.html",{"post": post},)
