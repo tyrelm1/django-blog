e9e21a14ab23bb7abebb5ab0228e16641f65fd56
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-02-29 21:30:47 +0000

    added post detail

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -28,6 +29,7 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    comment_form = CommentForm()
     
     return render(
         request,
@@ -36,5 +38,6 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )
